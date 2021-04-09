b55cc88de9baecec314e401f8a2ba7d46d6c5349
Antoine Planche <50099382+DowForProxy@users.noreply.github.com>
2021-04-09 19:33:48 +0200

    Add files via upload

---
+++ generationLargePrimeNumber.py
@@ -0,0 +1,42 @@
+#encoding utf-8
+
+import random
+import math
+
+'''
+def primalTest(p):
+    if p%2 == 0:
+        return False
+    else:
+        i = 3
+        sqrt = math.sqrt(p)
+        while(i<sqrt):
+            if p%i == 0:
+                return False
+    return True
+'''
+
+def primalFermatTest(p):
+    if pow(2,p-1)%p != 1:
+        return False
+    else:
+        return True
+        
+        '''
+        if primalTest(p) == True:
+            return True
+    return False
+        '''
+
+
+
+def generationLargePrimeNumber(n): # n is the number of bits you wish
+    while(True):
+        p = random.randint(pow(2,n-1),pow(2,n)-1)
+        if primalFermatTest(p) == True:
+            break
+    return p
+
+print("voici un nombre premier de 16 bits",generationLargePrimeNumber(16))
+
+
